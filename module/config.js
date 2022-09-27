6336e441425dfe63068331614b422932da4204de
Jose Vitor Vincoski <jose.vitor.abilhoa.vincoski@gmail.com>
2022-09-26 23:08:31 -0300

    Item sheet is working.

--- module/config.js
+++ module/config.js
@@ -1,20 +1,61 @@
 export const op_rpg = {};
 
+op_rpg.WeaponDamageTypes = {
+    Corte: "Corte",
+    Impacto: "Impacto",
+    Perfuracao: "Perfuração",
+    Balistico: "Balístico"
+}
+
 op_rpg.WeaponProficiencyTypes = {
-    ArmaSimples: "Arma Simples",
-    ArmaTatica: "Arma Tática",
-    ArmaPesada: "Arma Pesada"
+    ArmaSimples: "Simples",
+    ArmaTatica: "Tática",
+    ArmaPesada: "Pesada"
 }
 
 op_rpg.WeaponRangeTypes = {
     ArmaSimples: "Corpo a Corpo",
-    ArmaTatica: "Arremesso",
-    ArmaTatica: "Disparo",
-    ArmaTatica: "Fogo"
+    ArmaArremesso: "Arremesso",
+    ArmaDisparo: "Disparo",
+    ArmaFogo: "Fogo"
+}
+
+op_rpg.ItemCategory ={
+    Cat0: "Categoria: 0",
+    Cat1: "Categoria: I",
+    Cat2: "Categoria: II",
+    Cat3: "Categoria: III",
+    Cat4: "Categoria: IV"
 }
 
 op_rpg.WeaponHilt = {
     Leve: "Leve",
     UmaMao: "Uma Mão",
     DuasMaos: "Duas Mãos"
+}
+
+op_rpg.WeaponThreat = {
+    T18: 18,
+    T19: 19,
+    T20: 20    
+}
+
+op_rpg.WeaponCrit = {
+    Cx2: "x2",
+    Cx3: "x3",
+    Cx4: "x4"    
+}
+
+op_rpg.WeaponAgileStrong = {
+    nothing: "-",
+    Cx3: "Agil",
+    Cx4: "Forte"    
+}
+
+op_rpg.ItemSpace = {
+    S0: 0,
+    S1: 1,
+    S2: 2,
+    S3: 3,
+    S4: 4    
 }
--- module/sheets/OPItemSheet.js
+++ module/sheets/OPItemSheet.js
@@ -1,11 +1,12 @@
 export default class OPItemSheet extends ItemSheet {
     get template(){
-        return `systems/op-rpg/templates/sheets/${this.item.data.type}/${this.item.data.type}-sheet.html`
+        return `systems/op-rpg/templates/sheets/${this.item.type}/${this.item.type}-sheet.hbs`
     }
 
     getData(){
         const data = super.getData();
 
+        console.log(data);
         data.config = CONFIG.op_rpg;
 
         return data;
